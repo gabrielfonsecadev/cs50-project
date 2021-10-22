a8479d518d23f6184117d41236e06c2635f9477f
gabs-sf <93004749+gabs-sf@users.noreply.github.com>
2021-10-22 16:21:18 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int n;
+	do
+	{
+		printf ("Width? ");
+		char * fgets (n,4,stdin);//stdin significa que vai ler do teclado, e então, vai armazenar no "n"
+	}
+	while (n < 1);//só vai executar o codigo de baixo caso o valor dado pelo usuário for 1 ou maior que 1
+
+	for (int i = 0; i < 4; i++)
+	{
+		printf ("?");
+		printf ("/n");
+	}
+}
